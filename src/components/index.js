cfe721744010b6785a3b523abe83d41732306f99
Ayush Sahu <sahuayush0613@gmail.com>
2024-06-28 12:16:31 +0530

    Feat : Design user-authentication page

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import CustomButton from './CustomButton';
 import TextInput from './TextInput';
+import SignUp from './SignUp';
 export {
     Navbar,
     Footer, 
     CustomButton,
     TextInput,
+    SignUp,
 }
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -3,7 +3,7 @@ import { dispatch } from "./store";
 import { users } from "../utils/data";
 
 const initialState = {
-  user: JSON.parse(window?.localStorage.getItem("userInfo")) ?? users[1],
+  user:{}//JSON.parse(window?.localStorage.getItem("userInfo")) ?? users[1],
 };
 
 const userSlice = createSlice({
@@ -33,3 +33,4 @@ export function Logout() {
     dispatch(userSlice.actions.logout());
   };
 }
+
